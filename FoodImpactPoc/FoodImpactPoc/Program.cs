953cf7755d8b814c3ebd890687bc4bacf41067fe
sngov <sophanna.ngov@gmail.com>
2024-10-12 19:58:11 +0200

    Added sln and poc project

---
+++ FoodImpactPoc/FoodImpactPoc/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodImpactPoc;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Chopin, s'il te plait arrete de mordre!");
+
+
+    }
+}
